b2f8252c05ff9fafd477b36d594c9f5fef4346bd
duda <lacwerda@gmail.com>
2023-07-10 03:16:42 -0300

    files

--- src/for_nasm.asm
+++ src/for_nasm.asm
@@ -23,17 +23,19 @@ _start:
     ; Macro FOR_LOOP
     %macro FOR_LOOP 4
         mov %1, %2
-        cmp %1, %3
+        cmp %1, %3  ; compara parametros 1 e 3
+        ; vai pro final do loop caso parametro 1 seja maior que parametro 3
         jg %%end_loop
 
         %%loop:
             ; Coloque aqui o código que você deseja executar no loop
             mov eax, 1
-            call print_number
+            call print_number  ; imprime o numero no buffer
 
-            add %1, %4
-            cmp %1, %3
-            jle %%loop
+            add %1, %4  ; soma parametros 1 e 4
+            cmp %1, %3  ; compara parametros 1 e 3
+            ; vai pro comeco do loop caso parametro 1 seja menor ou igual que parametro 3
+            jle %%loop 
 
         %%end_loop:
     %endmacro
